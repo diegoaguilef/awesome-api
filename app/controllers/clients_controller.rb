800216c0a9cf9f70ada19e06bb3932c232a10c16
Diego Aguilef <diego.aguilef@gmail.com>
2018-03-09 19:30:59 -0300

    chore(clients): adds clients controller with some actions

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,22 @@
+class ClientsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def delete
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'clients/index'
+
+  get 'clients/new'
+
+  get 'clients/edit'
+
+  get 'clients/show'
+
+  get 'clients/delete'
+
+  get 'clients/create'
+
+  get 'clients/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
